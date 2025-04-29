ae6f04fa932fe5b28c7b2767d44648f46debc93c
Louin <134504671+LouinLee@users.noreply.github.com>
2025-04-29 21:43:58 +0800

    Cleanup 2

--- frontend/src/utils/axios.js
+++
@@ -1,9 +0,0 @@
-// src/utils/axios.js
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
-    withCredentials: true,
-});
-
-export default api;
